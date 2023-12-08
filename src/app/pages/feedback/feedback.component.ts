98ce13503235f5738c0324213482bd805e1935c3
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2023-12-09 01:34:39 +0200

    initialization angular CLI

---
+++ src/app/pages/feedback/feedback.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback',
+  standalone: true,
+  imports: [],
+  template: `feedback`,
+})
+export class FeedbackComponent {}
---
+++ src/app/pages/shop/shop.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  standalone: true,
+  imports: [],
+  template: `mait shop`,
+})
+export class ShopComponent {}
